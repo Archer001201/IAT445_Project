b1bf163970d7d165c617878247451c9d86b6291b
archer001201 <zhongchuqiao1201@gmail.com>
2024-10-15 01:22:25 -0700

    ...

--- Afterimage/Assets/Scripts/CameraMechanics/ShutterController.cs
+++ Afterimage/Assets/Scripts/CameraMechanics/ShutterController.cs
@@ -7,27 +7,56 @@ namespace CameraMechanics
     {
         public float maxRayDistance = 5f;
         public AudioClip shutterSfx;
+        public AudioClip beepSfx;
         public Camera virtualCamera;
+        public GameObject screenOutliner;
+        public bool isGrabbing;
         
         private AudioSource audioSource;
+        private bool canCapture;
         [SerializeField] private CaptureEvent captureEvent;
 
         private void Awake()
         {
             audioSource = GetComponent<AudioSource>();
+            screenOutliner.SetActive(false);
         }
 
         private void Update()
         {
+            if (!isGrabbing) return;
+            
             var rayOrigin = virtualCamera.transform.position;
             var rayDirection = virtualCamera.transform.forward;
 
-            if (Physics.Raycast(rayOrigin, rayDirection, out var hit, maxRayDistance, LayerMask.GetMask("Event Trigger"), QueryTriggerInteraction.Collide))
+            if (Physics.Raycast(rayOrigin, rayDirection, out var hit, maxRayDistance,
+                    LayerMask.GetMask("Event Trigger"), QueryTriggerInteraction.Collide))
             {
-                if (hit.transform.CompareTag("CaptureEventTrigger") && captureEvent == null)
-                    captureEvent = hit.transform.GetComponent<CaptureEvent>();
+                if (hit.transform.CompareTag("CaptureEventTrigger"))
+                {
+                    if (captureEvent == null)
+                    {
+                        captureEvent = hit.transform.GetComponent<CaptureEvent>();
+                    }
+                }
                 else captureEvent = null;
             }
+            else captureEvent = null;
+
+            canCapture = captureEvent != null && captureEvent.allKeyObjectsPlaced && captureEvent.noExtraObjectsPlaced;
+            if (canCapture)
+            {
+                if (!screenOutliner.activeSelf)
+                {
+                    screenOutliner.SetActive(true);
+                    if (audioSource != null && beepSfx != null)
+                    {
+                        audioSource.PlayOneShot(beepSfx);
+                    }
+                }
+            }
+            else if (screenOutliner.activeSelf) screenOutliner.SetActive(false);
+            
             Debug.DrawRay(rayOrigin, rayDirection * maxRayDistance, Color.red);
         }
 
@@ -41,6 +70,11 @@ namespace CameraMechanics
             if (captureEvent == null || !captureEvent.allKeyObjectsPlaced || !captureEvent.noExtraObjectsPlaced) return;
             
             captureEvent.onFinishedEvent.Invoke();
+        }
+
+        public void SetIsGrabbing(bool result)
+        {
+            isGrabbing = result;
         }
     }
 }
--- Afterimage/Assets/Scripts/Interactor/HoverUiWithTag.cs
+++ Afterimage/Assets/Scripts/Interactor/HoverUiWithTag.cs
@@ -12,8 +12,8 @@ namespace Interactor
         public string targetTag = "TargetTag";
         public LayerMask raycastLayers;
 
-        private static GameObject _currentHitObject = null;  // 静态变量，确保多个控制器共享
-        private static HoverUiWithTag _activeController = null; // 确保只有一个控制器在控制 UI
+        private static GameObject _currentHitObject;
+        private static HoverUiWithTag _activeController;
 
         private void Start()
         {
@@ -32,24 +32,19 @@ namespace Interactor
         {
             var ray = new Ray(transform.position, transform.forward);
 
-            // 检查是否有物体被命中
             if (Physics.Raycast(ray, out var hit, rayDistance, raycastLayers, QueryTriggerInteraction.Collide))
             {
-                // 确保命中目标标签物体
                 if (hit.collider.CompareTag(targetTag))
                 {
                     var hitObject = hit.collider.gameObject;
 
-                    // 如果当前没有其他控制器在处理，或者当前控制器为激活的控制器
                     if (_currentHitObject != hitObject || _activeController == this)
                     {
                         _currentHitObject = hitObject;
                         _activeController = this;
 
-                        // 如果 UI 是非激活状态，激活它
                         if (hoverUI != null && !hoverUI.activeSelf)
                         {
-                            Debug.Log(hitObject.name);
                             hoverUI.GetComponentInChildren<Image>().material =
                                 hitObject.GetComponent<MeshRenderer>().material;
 
@@ -70,13 +65,12 @@ namespace Interactor
 
         private void HideUI()
         {
-            // 只有当前控制器可以隐藏 UI
             if (hoverUI != null && hoverUI.activeSelf && _activeController == this)
             {
                 hoverUI.SetActive(false);
                 hoverUI.GetComponentInChildren<Image>().material = null;
-                _activeController = null;  // 释放控制器
-                _currentHitObject = null;  // 清除命中物体
+                _activeController = null;
+                _currentHitObject = null;
             }
         }
     }

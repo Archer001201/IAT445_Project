12f8e5c05ad24fb6fb71e57e9aef661c66d9b8a0
TimChenCSY <2760569447csy@gmail.com>
2024-12-07 22:48:31 -0800

    X

--- Afterimage/Assets/Scripts/Utilities/TimelineEventHandler.cs
+++ Afterimage/Assets/Scripts/Utilities/TimelineEventHandler.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Events;
@@ -9,50 +8,43 @@ public class TimelineEventHandler : MonoBehaviour
     public UnityEvent onTimelineEnd;         // 定义 UnityEvent
 
     private bool isTimelinePlaying = false;  // 标志是否正在播放 Timeline
-    private bool hasPlayed;
 
-    void OnEnable()
+    void Start()
     {
-        // if (playableDirector == null)
-        // {
-        //     Debug.LogError("PlayableDirector 未绑定！");
-        //     return;
-        // }
-        //
-        // // 注册 PlayableDirector 的状态更改事件
-        playableDirector.stopped += _ => onTimelineEnd?.Invoke();
+        if (playableDirector == null)
+        {
+            Debug.LogError("PlayableDirector 未绑定！");
+            return;
+        }
+
+        // 注册 PlayableDirector 的状态更改事件
+        playableDirector.stopped += OnTimelineStopped;
     }
 
-    // private void OnDisable()
-    // {
-    //     playableDirector.stopped -= _ => onTimelineEnd?.Invoke();
-    // }
+    void Update()
+    {
+        // 检测 Timeline 的播放状态（播放中且未完成）
+        if (playableDirector.state == PlayState.Playing && !isTimelinePlaying)
+        {
+            isTimelinePlaying = true;
+        }
+    }
 
-    // void Update()
-    // {
-    //     // 检测 Timeline 的播放状态（播放中且未完成）
-    //     if (playableDirector.state == PlayState.Playing && !hasPlayed)
-    //     {
-    //         hasPlayed = true;
-    //     }
-    // }
-    //
-    // private void OnTimelineStopped()
-    // {
-    //     // 当 Timeline 停止时（播放完成或手动停止）
-    //     // if (hasPlayed && director.stopped)
-    //     // {
-    //     //     // isTimelinePlaying = false;
-    //     //     onTimelineEnd?.Invoke(); // 触发 UnityEvent
-    //     // }
-    //     onTimelineEnd?.Invoke(); 
-    // }
-    //
-    // void OnDestroy()
-    // {
-    //     if (playableDirector != null)
-    //     {
-    //         playableDirector.stopped -= OnTimelineStopped; // 取消事件注册
-    //     }
-    // }
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+        // 当 Timeline 停止时（播放完成或手动停止）
+        if (isTimelinePlaying)
+        {
+            isTimelinePlaying = false;
+            onTimelineEnd?.Invoke(); // 触发 UnityEvent
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (playableDirector != null)
+        {
+            playableDirector.stopped -= OnTimelineStopped; // 取消事件注册
+        }
+    }
 }
